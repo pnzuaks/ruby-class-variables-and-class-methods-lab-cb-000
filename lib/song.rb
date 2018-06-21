7bfa645dacc1aec7a9aaaefddde71f813c51c7b3
Patricia Nykaza <pnykaza@calstatela.edu>
2018-06-21 18:49:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   
   def self.genre_count
     genre_hash = {}
-    @@genres.each {|i| genre_hash[i] = 0}
+    @@genres.each {|i| genre_hash[i] = 1}
    
     genre_hash.each  {|key, val|
       if genre_hash.key?(key) 
